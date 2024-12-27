dbabc634bdd0b79011c5b6812ced02d01f05555f
Ezira Ashenafi <eazash22@gmail.com>
2024-12-27 09:40:47 +0300

    fix: use svh height

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+/**
+ * @type {import('tailwindcss').Config}
+ */
 export default {
   theme: {
     extend: {
+      height: {
+        'screen': '100svh 100dvh'
+      },
       fontFamily: {
         'noto-ethiopic': ['Noto Sans Ethiopic', 'sans-serif']
       },
