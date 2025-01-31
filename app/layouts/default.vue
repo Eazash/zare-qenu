225e18134429b945c2eb9bf2e952dc3267344873
Ezira Ashenafi <eazash22@gmail.com>
2025-01-31 16:00:04 +0300

    feat: set html attributes from i18n

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const { $colorMode, $i18n} = useNuxtApp()
+const { $colorMode, $i18n } = useNuxtApp()
 const { state, next } = useCycleList([
   ['light', 'ic:sharp-light-mode',],
   ['dark', 'ic:sharp-dark-mode'],
@@ -7,6 +7,9 @@ const { state, next } = useCycleList([
 ], {
   initialValue: ['system', 'ic:round-monitor']
 })
+
+const i18nHead = useLocaleHead()
+
 const [selectedLocale, toggleLocale] = useToggle($i18n.locale.value, {
   truthyValue: 'en',
   falsyValue: 'am'
@@ -23,14 +26,27 @@ watchEffect(() => {
 </script>
 <template>
   <div class="min-h-screen flex flex-col p-4 md:p-8">
-    <nav class="flex justify-end items-center gap-2 md:gap-4">
-      <button @click="toggleLocale()" role="button" aria-label="Cycle between Languages">
-        <Icon name="ic:baseline-language" class="size-6 md:size-8"/>
-      </button>
-      <button @click="next()" aria-label="Cycle between color modes">
-        <Icon :name="state[1]!" class="size-6 md:size-8"/>
-      </button>
-    </nav>
-    <slot />
+    <Html :lang="i18nHead.htmlAttrs?.lang" :dir="i18nHead.htmlAttrs?.dir">
+      <Head>
+        <template v-for="link in i18nHead.link" :key="link.hid">
+          <Link :id="link.hid" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+        </template>
+        <template v-for="meta in i18nHead.meta" :key="meta.hid">
+          <Meta :id="meta.hid" :property="meta.property" :content="meta.content" />
+        </template>
+      </Head>
+
+      <Body>
+        <nav class="flex justify-end items-center gap-2 md:gap-4">
+          <button @click="toggleLocale()" role="button" aria-label="Cycle between Languages">
+            <Icon name="ic:baseline-language" class="size-6 md:size-8" />
+          </button>
+          <button @click="next()" aria-label="Cycle between color modes">
+            <Icon :name="state[1]!" class="size-6 md:size-8" />
+          </button>
+        </nav>
+        <slot />
+      </Body>
+    </Html>
   </div>
 </template>
