a712312da4a1649d72d1c33d535a6a6277f03b8c
Ezira Ashenafi <eazash22@gmail.com>
2025-01-31 15:59:17 +0300

    feat: add locale message

---
+++ i18n.config.ts
@@ -0,0 +1,12 @@
+export default defineI18nConfig(() => ({
+  messages: {
+    en: {
+      heading: 'Today According to the Ethiopic Calendar',
+      alt_lang_subheading: 'ዛሬ ቀኑ እንደ ኢትዮጵያውያን ቀን አቆጣጠር'
+    },
+    am: {
+      heading: 'ዛሬ ቀኑ እንደ ኢትዮጵያውያን ቀን አቆጣጠር',
+      alt_lang_subheading: 'Today According to the Ethiopic Calendar'
+    }
+  }
+}))
